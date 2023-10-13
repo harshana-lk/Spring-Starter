b0416fdea53c54180c023351b6eb2811b47b36cd
Harshana <mailtoharshanaonlive@gmail.com>
2023-10-13 19:21:08 +0530

    Introducing ShutdownHook

--- Core/src/main/java/me/harshu/AppInitializer.java
+++ Core/src/main/java/me/harshu/AppInitializer.java
@@ -7,6 +7,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class AppInitializer {
     public static void main(String[] args) {
 
+//        by this method we can run anything before shutdown the JVM
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("JVM is about to Shutdown");
+            }
+        }));
+
 //        Creating Bean Factory (AnnotationConfigApplicationContext Object)
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
 
@@ -21,6 +29,7 @@ public class AppInitializer {
         bean.testBean();
         System.out.println(bean);
 
+//        Closing the ApplicationContext
         context.close();
     }
 }
---
+++ Core/src/main/java/me/harshu/bean/SpringBeanTwo.java
@@ -0,0 +1,10 @@
+package me.harshu.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBeanTwo {
+    public SpringBeanTwo() {
+        System.out.println("Spring Bean Two Created");
+    }
+}
