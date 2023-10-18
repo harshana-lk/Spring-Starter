697c439498f88ed1994b00b8b99d09001d6bd4e5
Harshana <mailtoharshanaonlive@gmail.com>
2023-10-18 18:23:21 +0530

    Import Configurations into a single Class by @Import Annotation

--- 05_Configurations/src/main/java/me/harshu/AppInitializer.java
+++ 05_Configurations/src/main/java/me/harshu/AppInitializer.java
@@ -1,14 +1,12 @@
 package me.harshu;
 
 import me.harshu.config.AppConfig;
-import me.harshu.config.AppConfig1;
-import me.harshu.config.AppConfig2;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppInitializer {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.register(AppConfig.class, AppConfig1.class, AppConfig2.class);
+        context.register(AppConfig.class);
         context.refresh();
         context.registerShutdownHook();
     }
--- 05_Configurations/src/main/java/me/harshu/config/AppConfig.java
+++ 05_Configurations/src/main/java/me/harshu/config/AppConfig.java
@@ -2,8 +2,10 @@ package me.harshu.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan(basePackages = "me.harshu.bean")
+@Import({AppConfig1.class, AppConfig2.class}) //we can import configurations into a single class
 public class AppConfig {
 }
