c0f38e2cb9069d0a52066f5dd857ae879329503a
Harshana <mailtoharshanaonlive@gmail.com>
2023-10-18 16:55:44 +0530

    Light Mode

--- 04_FullMode_VS_LightMode/src/main/java/me/harshu/bean/SpringBean.java
+++ 04_FullMode_VS_LightMode/src/main/java/me/harshu/bean/SpringBean.java
@@ -37,4 +37,24 @@ public class SpringBean implements BeanNameAware, BeanFactoryAware, ApplicationC
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         System.out.println("SpringBean Application Context Aware ");
     }
+
+    @Bean
+    public MyBasicDataSource basicDataSource(){
+//        MyConnection myConnection = new MyConnection();
+//        Inter Bean Dependency Invocation
+        MyConnection myConnection1 = myConnection();
+        MyConnection myConnection2 = myConnection();
+        MyConnection myConnection3 = myConnection();
+        System.out.println(myConnection1);
+        System.out.println(myConnection2);
+        System.out.println(myConnection3);
+        MyBasicDataSource myBasicDataSource = new MyBasicDataSource();
+        myBasicDataSource.setConnection(myConnection1);
+        return myBasicDataSource;
+    }
+
+    @Bean
+    public MyConnection myConnection(){
+        return new MyConnection();
+    }
 }
--- 04_FullMode_VS_LightMode/src/main/java/me/harshu/config/AppConfig.java
+++ 04_FullMode_VS_LightMode/src/main/java/me/harshu/config/AppConfig.java
@@ -1,8 +1,5 @@
 package me.harshu.config;
 
-import me.harshu.bean.MyBasicDataSource;
-import me.harshu.bean.MyConnection;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,23 +7,5 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "me.harshu.bean")
 public class AppConfig {
 
-    @Bean
-    public MyBasicDataSource basicDataSource(){
-//        MyConnection myConnection = new MyConnection();
-//        Inter Bean Dependency Invocation
-        MyConnection myConnection1 = myConnection();
-        MyConnection myConnection2 = myConnection();
-        MyConnection myConnection3 = myConnection();
-        System.out.println(myConnection1);
-        System.out.println(myConnection2);
-        System.out.println(myConnection3);
-        MyBasicDataSource myBasicDataSource = new MyBasicDataSource();
-        myBasicDataSource.setConnection(myConnection1);
-        return myBasicDataSource;
-    }
 
-    @Bean
-    public MyConnection myConnection(){
-        return new MyConnection();
-    }
 }
