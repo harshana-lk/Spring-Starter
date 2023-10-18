e1798b52ed524b3a035177f63592c5395d617a7e
Harshana <mailtoharshanaonlive@gmail.com>
2023-10-18 14:09:06 +0530

    Introducing Inter Bean Dependency

--- 04_FullMode_VS_LightMode/src/main/java/me/harshu/bean/SpringBeanTwo.java
+++ 04_FullMode_VS_LightMode/src/main/java/me/harshu/bean/SpringBeanTwo.java
@@ -5,33 +5,33 @@ import org.springframework.beans.factory.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
-public class SpringBeanOne implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
-    public SpringBeanOne() {
-        System.out.println("Spring Bean One Object Created");
+public class SpringBeanTwo implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
+    public SpringBeanTwo() {
+        System.out.println("Spring Bean Two Object Created");
     }
 
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        System.out.println("SpringBeanOne Bean Factory Aware");
+        System.out.println("SpringBeanTwo Bean Factory Aware");
     }
 
     @Override
     public void setBeanName(String name) {
-        System.out.println("SpringBeanOne Bean Name Aware");
+        System.out.println("SpringBeanTwo Bean Name Aware");
     }
 
     @Override
     public void destroy() throws Exception {
-        System.out.println("SpringBeanOne Bean Disposable Bean");
+        System.out.println("SpringBeanTwo Bean Disposable Bean");
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        System.out.println("SpringBeanOne Bean Initializing Bean Aware");
+        System.out.println("SpringBeanTwo Bean Initializing Bean Aware");
     }
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println("SpringBeanOne Bean Application Context Aware");
+        System.out.println("SpringBeanTwo Bean Application Context Aware");
     }
 }
--- 04_FullMode_VS_LightMode/src/main/java/me/harshu/config/AppConfig.java
+++ 04_FullMode_VS_LightMode/src/main/java/me/harshu/config/AppConfig.java
@@ -1,9 +1,29 @@
 package me.harshu.config;
 
+import me.harshu.bean.SpringBeanOne;
+import me.harshu.bean.SpringBeanTwo;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "me.harshu.bean")
 public class AppConfig {
+
+    @Bean
+    public SpringBeanOne getBeanOne(){
+//        Inter Bean Dependency
+        SpringBeanTwo bean = getBeanTwo();
+        SpringBeanTwo beanTwo = getBeanTwo();
+        System.out.println(bean);
+        System.out.println(beanTwo);
+
+
+        return new SpringBeanOne();
+    }
+
+    @Bean
+    public SpringBeanTwo getBeanTwo(){
+        return new SpringBeanTwo();
+    }
 }
