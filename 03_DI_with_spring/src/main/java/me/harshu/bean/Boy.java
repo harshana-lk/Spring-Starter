70be319d21624bf92dc7536ea88fec1e68840a80
Harshana <mailtoharshanaonlive@gmail.com>
2023-10-18 12:32:40 +0530

    @Qualifier Annotation Introduction

--- 03_DI_with_spring/src/main/java/me/harshu/bean/Boy.java
+++ 03_DI_with_spring/src/main/java/me/harshu/bean/Boy.java
@@ -1,10 +1,12 @@
 package me.harshu.bean;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Boy {
+    @Qualifier("girlOne")
     @Autowired
     GoodGirl girl;
 
--- 03_DI_with_spring/src/main/java/me/harshu/bean/GirlTwo.java
+++ 03_DI_with_spring/src/main/java/me/harshu/bean/GirlTwo.java
@@ -1,8 +1,10 @@
 package me.harshu.bean;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class GirlTwo implements GoodGirl {
     public GirlTwo() {
         System.out.println("Girl Object Created");
