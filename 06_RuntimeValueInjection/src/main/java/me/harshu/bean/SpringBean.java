badc59ca11f029cf3036ebf3b40b7f80aaaed2b2
Harshana <mailtoharshanaonlive@gmail.com>
2023-10-18 21:51:30 +0530

    @Value Annotation Introduced

--- 06_RuntimeValueInjection/src/main/java/me/harshu/bean/SpringBean.java
+++ 06_RuntimeValueInjection/src/main/java/me/harshu/bean/SpringBean.java
@@ -1,10 +1,13 @@
 package me.harshu.bean;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBean {
-    public SpringBean() {
+
+    public SpringBean(@Value("C001") String id) {
         System.out.println("Spring Bean Object Created");
+        System.out.println(id);
     }
 }
