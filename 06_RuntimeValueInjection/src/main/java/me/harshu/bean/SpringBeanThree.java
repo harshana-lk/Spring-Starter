9b2fa96ef30d91ee8103999956113b83ba12dd6f
Harshana <mailtoharshanaonlive@gmail.com>
2023-10-19 11:32:09 +0530

    Introduced Spring Environment Module

---
+++ 06_RuntimeValueInjection/src/main/java/me/harshu/bean/SpringBeanThree.java
@@ -0,0 +1,10 @@
+package me.harshu.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBeanThree {
+    public SpringBeanThree() {
+        System.out.println("SpringBeanThree Object Created ");
+    }
+}
---
+++ SpringEnvironment/src/main/java/me/harshu/config/AppConfig.java
@@ -0,0 +1,9 @@
+package me.harshu.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "me.harshu.bean")
+public class AppConfig {
+}
