1e3bfe419fca14d312fee4b68440bbe570bf2c22
Harshana <mailtoharshanaonlive@gmail.com>
2023-10-19 14:49:38 +0530

    Added Servlet API to the Spring MVC

--- 08_Spring_MVC/src/main/java/me/harshu/bean/SpringBean.java
+++ 08_Spring_MVC/src/main/java/me/harshu/bean/SpringBean.java
@@ -1,4 +1,10 @@
 package me.harshu.bean;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SpringBean {
+    public SpringBean() {
+        System.out.println("Spring Bean Object Created");
+    }
 }
