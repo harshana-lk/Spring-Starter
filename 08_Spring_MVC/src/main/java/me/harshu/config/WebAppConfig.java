bd376341a3a775be6efbb68afc2d9072f2897530
Harshana <mailtoharshanaonlive@gmail.com>
2023-10-19 14:04:09 +0530

    Updated WebAppConfig Class

--- 08_Spring_MVC/src/main/java/me/harshu/config/WebAppConfig.java
+++ 08_Spring_MVC/src/main/java/me/harshu/config/WebAppConfig.java
@@ -1,4 +1,11 @@
 package me.harshu.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = "me.harshu.bean")
+@EnableWebMvc
 public class WebAppConfig {
 }
