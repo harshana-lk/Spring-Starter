3c0c1d936f4da257c72825cc282ea66bd67bbf34
Harshana <mailtoharshanaonlive@gmail.com>
2023-10-19 11:47:20 +0530

    Getting System Environment Variables by System.getenv();

--- 07_SpringEnvironment/src/main/java/me/harshu/AppInitializer.java
+++ 07_SpringEnvironment/src/main/java/me/harshu/AppInitializer.java
@@ -3,11 +3,19 @@ package me.harshu;
 import me.harshu.config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Map;
+
 public class AppInitializer {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.register(AppConfig.class);
         context.refresh();
         context.registerShutdownHook();
+
+//        How to get System Environment Variables
+        Map<String, String> getenv = System.getenv();
+        for (String key : getenv.keySet()) {
+            System.out.println(key+" : "+getenv.get(key));
+        }
     }
 }
