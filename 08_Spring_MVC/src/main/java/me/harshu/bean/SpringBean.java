16f7df142336b866c84e700c9d2f8bc6c3ef786e
Harshana <mailtoharshanaonlive@gmail.com>
2023-10-19 21:44:26 +0530

    Adding Controllers to the Application

--- 08_Spring_MVC/src/main/java/me/harshu/bean/SpringBean.java
+++ 08_Spring_MVC/src/main/java/me/harshu/bean/SpringBean.java
@@ -1,10 +1,39 @@
 package me.harshu.bean;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBean {
+public class SpringBean implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
     public SpringBean() {
         System.out.println("Spring Bean Object Created");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Spring Bean Factory Aware");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Spring Bean Name Aware");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Spring Bean Disposable Bean");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Spring Bean Initializing Bean");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Spring Bean Application Context Aware");
     }
 }
