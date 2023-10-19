46c3f2b3da3655640a68b95a8952140900591cb6
Harshana <mailtoharshanaonlive@gmail.com>
2023-10-19 12:34:46 +0530

    Introducing Resource Bundle to the Spring Environment

--- 07_SpringEnvironment/src/main/java/me/harshu/bean/SpringBeanOne.java
+++ 07_SpringEnvironment/src/main/java/me/harshu/bean/SpringBeanOne.java
@@ -6,14 +6,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBeanOne implements InitializingBean {
-    @Value("${user.name}")
+    @Value("${os.name}")
     private String osName;
+    @Value("${user.name}")
+    private String userName;
+    @Value("${ijse.application.name}")
+    private String projectName;
     public SpringBeanOne() {
         System.out.println("SpringBeanOne Object Created");
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        System.out.println("User Name : "+osName);
+        System.out.println("OS Name : "+osName);
+        System.out.println("User Name : "+userName);
+        System.out.println("Application Name : "+projectName);
     }
 }
--- 07_SpringEnvironment/src/main/java/me/harshu/config/AppConfig.java
+++ 07_SpringEnvironment/src/main/java/me/harshu/config/AppConfig.java
@@ -2,8 +2,10 @@ package me.harshu.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = "me.harshu.bean")
+@PropertySource("classpath:application.properties")
 public class AppConfig {
 }
