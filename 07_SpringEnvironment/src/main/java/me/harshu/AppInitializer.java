127617a014f6f69e9457dc65744470fe19f7dda4
Harshana <mailtoharshanaonlive@gmail.com>
2023-10-19 12:04:40 +0530

    Getting Predefined Properties from Java by System.getProperties();

--- 07_SpringEnvironment/src/main/java/me/harshu/AppInitializer.java
+++ 07_SpringEnvironment/src/main/java/me/harshu/AppInitializer.java
@@ -4,6 +4,7 @@ import me.harshu.config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Map;
+import java.util.Properties;
 
 public class AppInitializer {
     public static void main(String[] args) {
@@ -15,7 +16,14 @@ public class AppInitializer {
 //        How to get System Environment Variables
         Map<String, String> getenv = System.getenv();
         for (String key : getenv.keySet()) {
-            System.out.println(key+" : "+getenv.get(key));
+            System.out.println(key + " : " + getenv.get(key));
+        }
+
+//        How to get Predefined Properties from Java
+        System.out.println("==================================================================");
+        Properties properties = System.getProperties();
+        for (Object key : properties.keySet()) {
+            System.out.println(key+" : "+properties.get(key));
         }
     }
 }
