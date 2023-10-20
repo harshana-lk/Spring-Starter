035aa5b757ed6c1123fd9c1c6f7a8c828cc95d92
Harshana <mailtoharshanaonlive@gmail.com>
2023-10-20 17:24:22 +0530

    Json with various Handler Mapping methods

--- 09_Rest_Controllers/src/main/java/me/harshu/controller/JsonController.java
+++ 09_Rest_Controllers/src/main/java/me/harshu/controller/JsonController.java
@@ -2,22 +2,31 @@ package me.harshu.controller;
 
 import me.harshu.dto.CustomerDTO;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("json")
 public class JsonController {
 
     @PostMapping
-    public String getJsonRequest(CustomerDTO customerDTO) {
+    public String getJsonRequest(@RequestBody CustomerDTO customerDTO) {
         return "Hello Json : "+customerDTO.toString();
     }
+
     @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
     public CustomerDTO sendBackJson() {
         return new CustomerDTO("I001","Harshana","Badulla",150000.00);
+    }
+
+    @GetMapping(path = "array",produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ArrayList<CustomerDTO> sendBackJsonArray() {
+        ArrayList<CustomerDTO> arrayList = new ArrayList<>();
+        arrayList.add(new CustomerDTO("I001","Harshana","Badulla",150000.00));
+        arrayList.add(new CustomerDTO("I001","Harshana","Badulla",150000.00));
+        arrayList.add(new CustomerDTO("I001","Harshana","Badulla",150000.00));
+        return arrayList ;
     }
 
 }
