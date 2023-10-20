a3488013f105a70da6b0c5cb75f6b0fcf43ed065
Harshana <mailtoharshanaonlive@gmail.com>
2023-10-20 13:56:08 +0530

    Changing params types and names for testing

--- 09_Rest_Controllers/src/main/java/me/harshu/controller/ParamsController.java
+++ 09_Rest_Controllers/src/main/java/me/harshu/controller/ParamsController.java
@@ -8,15 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("params")
 public class ParamsController {
 
-    @GetMapping(params = {"param01","param02"})//handler mapping
-    public String test01() {
-        System.out.println("Hello Param 01");
-        return "Hello Param 01";
+    @GetMapping(params = {"id","name"})//handler mapping
+    public String test01(String id,String name) {
+        System.out.println("Hello Param 01 : "+id+" "+name);
+        return "Hello Param 01 : "+id+" "+name;
     }
 
     @GetMapping()//handler mapping
     public String test02() {
         System.out.println("Hello Param 02");
         return "Hello Param 02";
+    }
+
+    @GetMapping(params = {"name","salary"})//handler mapping
+    public String test01(String name,double salary) {
+        System.out.println("Hello Param 01 : "+name+" "+salary);
+        return "Hello Param 01 : "+name+" "+salary;
     }
 }
